9ffbe46ba471f9cae84c4d5ac2ae7535bcc3c045
Pere Josep Ferrer Ventura <pere.ferrer.pf@gmail.com>
2020-03-04 11:35:09 +0100

    Se añade layouts para el registro y la estructura de carpetas de la activity. Se asigna como vista principal login/registro

---
+++ app/src/main/java/ventura/ferrer/josep/pere/proyectofinalandroid/LoadingDialogFragment.kt
@@ -0,0 +1,42 @@
+package ventura.ferrer.josep.pere.proyectofinalandroid
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.dialog_loading.view.*
+
+const val ARG_MESSAGE = "message"
+
+class LoadingDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_loading, container)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val message = arguments?.getString(ARG_MESSAGE) ?: getString(
+            R.string.label_loading
+        )
+        view.labelMessage.text = message
+    }
+
+    companion object {
+        fun newInstance(message: String) : LoadingDialogFragment {
+            val fragment = LoadingDialogFragment()
+            val args = Bundle()
+            args.putString(ARG_MESSAGE, message)
+
+            fragment.arguments = args
+
+            return fragment
+        }
+    }
+}
---
+++ app/src/main/java/ventura/ferrer/josep/pere/proyectofinalandroid/feature/Login/view/ui/LoginActivity.kt
@@ -0,0 +1,13 @@
+package ventura.ferrer.josep.pere.proyectofinalandroid.feature.Login.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ventura.ferrer.josep.pere.proyectofinalandroid.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
